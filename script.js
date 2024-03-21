68dd08cb2ad8369f76f5afa93f5b85782bd58753
wisnawa <putu.wisnawa@gmail.com>
2024-03-21 19:23:22 +0800

    modified

--- script.js
+++ script.js
@@ -101,3 +101,13 @@ $(".testimonials-container").owlCarousel({
     },
   },
 });
+// Login
+const wrapper = document.querySelector(".wrapper"),
+  signupHeader = document.querySelector(".signup header"),
+  loginHeader = document.querySelector(".login header");
+loginHeader.addEventListener("click", () => {
+  wrapper.classList.add("active");
+});
+signupHeader.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
